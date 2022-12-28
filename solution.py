783f7cc31391d904cf6a0e0cb113c3e9eb5d1f77
D-coder <110387042+dcoder201@users.noreply.github.com>
2022-12-28 12:47:25 +0530

    Create scolution.py

---
+++ solution.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+class Solution:
+    def solve (self, L, R):
+        ans=0
+        for i in range(0,63):
+            for j in range(i+1,63):
+                for k in range(j+1,63):
+                    curr=0
+                    curr|=(1<<i)
+                    curr|=(1<<j)
+                    curr|=(1<<k)
+                    if curr>=L and curr<=R:
+                        ans+=1
+        return ans
+        # code here
+    def precompute (self):
+        pass
+        # code here
+        
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__': 
+    ob = Solution()
+    ob.precompute()
+    t = int (input ())
+    for _ in range (t):
+        L, R = map(int,input().split())
+        ans = ob.solve(L, R);
+        print(ans)
+
+
+
+
+# } Driver Code Ends
